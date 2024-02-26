0916de2d21a5578393ab608aae6f9328b889d911
11Johan11 <joso0412@hotmail.com>
2024-02-26 16:49:23 +0100

    Fixed health endpoint

--- src/models/mongo_api/server/server.mjs
+++ src/models/mongo_api/server/server.mjs
@@ -4,7 +4,8 @@ import "./loadEnvironment.mjs";
 import users from "./routes/users.mjs";
 import invites from "./routes/invites.mjs";
 import upload from "./routes/upload.mjs";
-import health from "./routes/health.mjs";
+import { db, client } from './db/conn.mjs';
+import { ObjectId } from "mongodb";
 
 const PORT = process.env.PORT || 5050;
 const app = express();
@@ -21,6 +22,31 @@ app.use("/api/health", upload);
 app.get('/status', (req, res) => {
   res.status(200).send('API is up and running');
 });
+
+app.get('/health', async (req, res) => {
+  try {
+    // Försök ansluta till databasen
+    await client.connect();
+
+    // Använd findOne för att testa databasanslutningen
+    const collection = db.collection("users");
+    const user = await collection.findOne({});
+
+    if (user) {
+      res.status(200).send('Database connection is healthy');
+    } else {
+      res.status(200).send('Database connection is healthy but no users found');
+    }
+  } catch (error) {
+    console.error('Database connection failed:', error);
+    res.status(500).send('Database connection failed');
+  } finally {
+    // Stäng anslutningen efter kontrollen
+    await client.close();
+  }
+});
+
+
 
 
 // start the Express server
