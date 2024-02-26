df6da343140e4c4df2ac522a862f6fa0416e3392
11Johan11 <joso0412@hotmail.com>
2024-02-26 16:09:51 +0100

    Fixed probes and health check endpoints for the second container

--- src/models/mongo_api/server/server.mjs
+++ src/models/mongo_api/server/server.mjs
@@ -4,6 +4,7 @@ import "./loadEnvironment.mjs";
 import users from "./routes/users.mjs";
 import invites from "./routes/invites.mjs";
 import upload from "./routes/upload.mjs";
+import health from "./routes/health.mjs";
 
 const PORT = process.env.PORT || 5050;
 const app = express();
@@ -14,6 +15,13 @@ app.use(express.json());
 app.use("/api/users", users);
 app.use("/api/invites", invites);
 app.use("/api/upload", upload);
+app.use("/api/health", upload);
+
+
+app.get('/status', (req, res) => {
+  res.status(200).send('API is up and running');
+});
+
 
 // start the Express server
 app.listen(PORT, () => {
